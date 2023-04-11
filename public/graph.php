956461da1ec7c063401eabbc624960c7bd5a2e3d
arnamp <129734128+arnamp@users.noreply.github.com>
2023-04-11 18:38:21 +0700

    Add files via upload

--- public/graph.php
+++ public/graph.php
@@ -139,7 +139,7 @@ $con  = mysqli_connect("containers-us-west-29.railway.app:7830","root","LOPC3n2w
                                 datasets: [{
                                     label:'Temp & Humid 1',
                                     backgroundColor:[
-                                    'RGB(0, 0, 255,0.1)'],borderColor:["RGB(255, 99, 71)"],borderWidth:2,
+                                    'RGB(0, 0, 255,0.1)'],borderColor:["RGB(255, 99, 71)"],
                                     data:<?php echo json_encode($info1); ?>,
                                 }]  
                               },
